3be179a81089c1b5bad83f63e190161acf2ece2b
GerricVill <qgavillanueva@tip.edu.ph>
2023-10-03 07:54:42 +0000

    Python codes

---
+++ test.py
@@ -0,0 +1,22 @@
+import math
+
+def test_sqr():
+    num = 9
+    assert math.sqrt(num) == 3
+
+def test_square():
+    num = 7
+    assert num*num == 49
+
+def test_check():
+    x = 100
+    assert x >=100
+
+def test_fun():
+    num = 5
+    assert num+1 == 6
+
+def test_multiply():
+    num1 = 1
+    num2 = 6
+    assert num1*num2 == 6
